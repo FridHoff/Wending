5024860e76cffe48dde4a5950b72980e45ffffa2
Flex2 <Flex2@DESKTOP-9CMC32C>
2025-01-20 17:53:13 +0300

    start

---
+++ Vending/WendingMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending
+{
+    internal class WendingMachine
+    {
+        int[] balance = { 100, 50, 25, 10 };
+        int[] money = { 1, 2, 5, 10 };
+        string[] goods = { "fanta", "sprite", "cola", "pepsy" };
+        int[] goodsAmount = new int[3];
+        void AddMoney(int amount, int id)
+        {
+            balance[id] += amount;
+        }
+        void BuyGood(int id, int amount)
+        {
+            goodsAmount[id] -= amount;           
+        }
+
+    }
+}
